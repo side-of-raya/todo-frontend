756fafa4785d53df5e04fd59ddb7e945a1e43436
user <gitlinden@gmail.com>
2019-10-14 18:19:54 +0300

    Changed entry point from main page (App) to authorization page (Login)

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Login from './Login'
+import * as serviceWorker from './serviceWorker';
+
+ReactDOM.render(<Login />, document.getElementById('root'));
+serviceWorker.unregister();
